d4b67cb5fecf683c1a875d1189d6751dec4e05e3
boxops <olahbalazs@outlook.com>
2022-11-05 14:24:00 +0000

    docstring and key file check functionality

--- operations/junos_public_key_auth_setup.sh
+++ operations/junos_public_key_auth_setup.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-# Purpose: Silently generate an SSH key and append it to a remote Juniper node.
+# Bash Docstring (Not POSIX compliant)
+DOC_REQUEST=70
+if [ "$1" = "-h" -o "$1" = "--help"]; then
+    cat <<DOCUMENTATION
+Purpose:
 
-# Requirements: The script assumes that the local DevBox has SSH access to the target Juniper node.
+    Silently generate an SSH key and append it to a remote Juniper node.
 
-# Usage: Execute without sudo permissions.
-# ./public_key_auth_setup.sh
+Requirements:
+
+    The script assumes that the local DevBox has SSH access to the target
+    Juniper node with username and password authentication.
+
+Usage:
+
+    Execute without sudo permissions:
+    ./public_key_auth_setup.sh
+
+DOCUMENTATION
+exit $DOC_REQUEST
+fi
+
+# User prompts
+DEFAULTREMOTEIP="10.1.1.1"
+echo "Remote host IP or domain name:"
+echo "Press enter for [$DEFAULTREMOTEIP]"
+read REMOTEIP
 
 DEFAULTSSHFILENAME="id_rsa"
 echo "New SSH key filename:"
 echo "Press enter for [$DEFAULTSSHFILENAME]"
 read SSHFILENAME
-
-DEFAULTREMOTEIP="10.1.1.1"
-echo "Remote host IP:"
-echo "Press enter for [$DEFAULTREMOTEIP]"
-read REMOTEIP
 
 DEFAULTREMOTEUSERNAME="admin"
 echo "Remote host username:"
@@ -27,18 +43,24 @@ echo "Remote host absolute filepath:"
 echo "Press enter for [$DEFAULTREMOTEFILEPATH]"
 read REMOTEFILEPATH
 
-cd ~/.ssh
+# Check if the key exists, create it if not
+if [ ! -f ~/.ssh/$SSHFILENAME ] || [ ! -f ~/.ssh/$DEFAULTSSHFILENAME ]; then
+    cd ~/.ssh
+    ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/${SSHFILENAME:=$DEFAULTSSHFILENAME} <<<y >/dev/null 2>&1
+    eval $(ssh-agent -s)
+    ssh-add ~/.ssh/${SSHFILENAME:=$DEFAULTSSHFILENAME}
+    echo "[ SSH key created. ]"
+elif [ -f ~/.ssh/$SSHFILENAME ] || [ -f ~/.ssh/$DEFAULTSSHFILENAME ]; then
+    echo "[ SSH key exists. ]"
+else
+    echo "[ Something went wrong. ]"
+fi
 
-ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/${SSHFILENAME:=$DEFAULTSSHFILENAME} <<<y >/dev/null 2>&1
-
-eval $(ssh-agent -s)
-
-ssh-add ~/.ssh/${SSHFILENAME:=$DEFAULTSSHFILENAME}
-
+echo "Uploading the key to the Juniper..."
 scp ~/.ssh/${SSHFILENAME:=$DEFAULTSSHFILENAME}.pub ${REMOTEUSERNAME:=$DEFAULTREMOTEUSERNAME}@${REMOTEIP:=$DEFAULTREMOTEIP}:${REMOTEFILEPATH:=$DEFAULTREMOTEFILEPATH}
 
 echo ""
-echo "[ SSH Key created. Follow the steps below to setup the Juniper for public key authentication. ]"
+echo "[ Follow the steps below to setup the Juniper for public key authentication. ]"
 echo "1. Assign the ssh key to the user on the Juniper:"
 echo "set system login user ${REMOTEUSERNAME:=$DEFAULTREMOTEUSERNAME} class super-user authentication load-key-file ${REMOTEFILEPATH:=$DEFAULTREMOTEFILEPATH}${SSHFILENAME:=$DEFAULTSSHFILENAME}.pub"
 echo "2. Test your key from your DevBox:"
