25003e381ca7c8247c7509ad694aeeb2b7b37a53
boxops <olahbalazs@outlook.com>
2022-11-08 20:37:51 +0000

    ssh auth key setup script

--- playbooks/configurations/junos_public_key_auth_setup.py
+++ playbooks/configurations/junos_public_key_auth_setup.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
+
 import os
 import subprocess
 from getpass import getpass
-from jnpr.junos import Device
-from jnpr.junos.utils.scp import SCP
-from jnpr.junos.utils.config import Config
+from netmiko import ConnectHandler, file_transfer
+
+'''
+Purpose:
+
+    Silently generate an SSH key and append it to a remote Juniper node.
+
+Requirements:
+
+    The script assumes that the local Ansible control node has SSH access
+    to the target Juniper node with username and password authentication.
+
+Usage:
+
+    Run the script without sudo permissions:
+    python3 public_key_auth_setup.py
+'''
 
 # User prompts
 default_remote_ip = "10.1.1.1"
@@ -12,17 +27,17 @@ remote_ip = input(f"Remote host IP or domain name [{default_remote_ip}]: ") or d
 
 default_ssh_filename = "id_rsa"
 ssh_file_name = input(f"New SSH key filename [{default_ssh_filename}]: ") or default_ssh_filename
+public_ssh_file_name = f"{ssh_file_name}.pub"
 
-default_remote_username = "admin"
+default_remote_username = "ansible"
 remote_username = input(f"Remote host username [{default_remote_username}]: ") or default_remote_username
 
 remote_password = getpass("Remote host password: ")
 
-default_remote_file_path = "/var/home/admin/"
+default_remote_file_path = "/var/home/ansible/"
 remote_file_path = input(f"Remote host absolute filepath [{default_remote_file_path}]: ") or default_remote_file_path
 
-
-# Check if the key exists, create it if not
+# Check if the SSH key exists, create it if not
 key_path = os.path.expanduser(f"~/.ssh/{ssh_file_name}")
 file_exists = os.path.exists(key_path)
 
@@ -32,18 +47,28 @@ if file_exists is False:
 else:
     print("Key exists.")
 
-# Copy over the local public key to the Junos
-# subprocess.run(["scp", f"{key_path}.pub", f"{remote_username}@{remote_ip}:{remote_file_path}"])
+device = {
+    "device_type": "juniper_junos",
+    "host": remote_ip,
+    "username": remote_username,
+    "password": remote_password,
+    "port": 22
+}
 
-device_port = "830"
-device = Device(host=default_remote_ip, user=remote_username, password=remote_password, port=device_port)
-with SCP(device) as scp:
-    scp.put(f"{key_path}.pub", remote_path=remote_file_path)
+# Copy over the local public SSH key to the Juniper
+connect = ConnectHandler(**device)
 
-# Assign the SSH key to the user on the Junos (configuration)
-set_command = f"set system login user {remote_username} class super-user authentication load-key-file {remote_file_path}{ssh_file_name}.pub"
-with Config(device, mode="private") as cu:
-    cu.open()
-    cu.load(set_command)
-    cu.commit()
-device.close()
+transfer = file_transfer(
+    connect,
+    source_file=f"{key_path}.pub",
+    dest_file=public_ssh_file_name,
+    file_system=remote_file_path,
+    direction="put",
+    overwrite_file=True
+)
+
+# Assign the SSH key to the user on the Juniper (configuration)
+command = f"set system login user {remote_username} class super-user authentication load-key-file {remote_file_path}{ssh_file_name}.pub"
+config = connect.send_config_set(command)
+config = connect.commit()
+print(config)
